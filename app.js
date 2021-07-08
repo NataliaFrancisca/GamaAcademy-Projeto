4774cddf0830956c905cc5659c58cfac2f8a27f1
NataliaFrancisca <nathifrancisca@gmail.com>
2021-07-07 21:36:21 -0300

    organizando o código

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 // pegar um input do usuário
 // quer escolher a música pelo idioma
 
-
-//Se a resposta for sim, mostrar os idiomas disponívei
+//Se a resposta for sim, mostrar os idiomas disponíveis
 //Pergunta qual catégoria que ela escolhe
 //Se a resposta for não, mostrar todas as músicas em ordem alfabetica dos autores
 
@@ -10,13 +9,13 @@ const musicas = require('./database');
 const readLine = require('readline-sync');
 
 
-const entradaInicial = readLine.question('Deseja buscar uma musica? S/N ');
+const entradaInicial = readLine.question('Deseja buscar musicas pelo idioma? S/N ');
 
 
 if(entradaInicial.toLocaleUpperCase() === 'S'){
 
-	console.log('Esses são os idiomas disponiveis: ')
-	console.log('Portugues ', 'Ingles ', 'Frances ', 'Italiano ', 'Coreano' ,'\n');
+	console.log('\n')
+	console.log('Esses são os idiomas disponiveis: ', 'Portugues ', 'Ingles ', 'Frances ', 'Italiano ', 'Coreano' ,'\n')
 
 	const entradaCategoria = readLine.question('Qual idioma voce escolhe:');
 	const retorno = musicas.filter(musica => musica.idioma === (entradaCategoria[0].toLocaleUpperCase() + entradaCategoria.substr(1)));
